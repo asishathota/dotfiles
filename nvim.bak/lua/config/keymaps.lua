56bc7d2687b2a69f169e1dcb81f3481433a1ddb3
asish <asishathota@gmail.com>
2025-10-26 02:32:15 +0000

    nvim -> nvim.bak

--- nvim.bak/lua/config/keymaps.lua
+++ nvim.bak/lua/config/keymaps.lua
@@ -1,44 +1,39 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>ee", vim.cmd.Ex)
-
 local keymap = vim.keymap
 
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
-keymap.set("n", "<leader>ll", "<cmd>Lazy<CR>", { desc = "Lazy interface" })
+keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Write file" })
 
--- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>so", "<C-w>v<cmd>Telescope find_files<cr>", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>ee", "<cmd>Ex<CR>")
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=v", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>so", "<C-w>v<cmd>Telescope find_files<cr>", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
--- better up/down
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
 keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
 keymap.set({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
 keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 keymap.set({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 
--- move lines
-vim.keymap.set({"i", "n"}, "<A-z>", "<cmd>set wrap!<CR>", { desc = "toggle wrap text"})
+keymap.set({ "i", "n" }, "<A-z>", "<cmd>set wrap!<CR>", { desc = "toggle wrap text" })
 
--- Resize window using <ctrl> arrow keys
-keymap.set("n", "<C-Up>", "<cmd>resize -2<cr>", { desc = "Increase Window Height" })
-keymap.set("n", "<C-Down>", "<cmd>resize +2<cr>", { desc = "Decrease Window Height" })
+keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Increase Window Height" })
+keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Decrease Window Height" })
 keymap.set("n", "<C-Left>", "<cmd>vertical resize +2<cr>", { desc = "Decrease Window Width" })
 keymap.set("n", "<C-Right>", "<cmd>vertical resize -2<cr>", { desc = "Increase Window Width" })
 
--- Move Lines
 keymap.set("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
 keymap.set("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
 keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
@@ -46,24 +41,13 @@ keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
 keymap.set("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
 keymap.set("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
 
--- Fuzzy finding
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-keymap.set("n", "<leader>en", function()
-  require("telescope.builtin").find_files({
-    cwd = vim.fn.stdpath("config"),
-  })
-end, { desc = "Goto config" })
-
--- diagnostic
 local diagnostic_goto = function(next, severity)
-  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
-  severity = severity and vim.diagnostic.severity[severity] or nil
-  return function()
-    go({ severity = severity })
-  end
+    return function()
+        vim.diagnostic.jump({
+            count = next and 1 or -1,
+            severity = severity and vim.diagnostic.severity[severity] or nil,
+        })
+    end
 end
 
 keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
@@ -74,22 +58,6 @@ keymap.set("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
 keymap.set("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
 keymap.set("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
 
--- save file
 keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
 
--- List all keymaps with a custom command
-vim.api.nvim_create_user_command('ShowKeymaps', function()
-  print("Keymaps:")
-  -- Get all normal mode keymaps (you can change 'n' to 'i', 'v', etc. based on the mode)
-  local keymaps = vim.api.nvim_get_keymap('n')
-  
-  for _, keymap in ipairs(keymaps) do
-    -- Only print keymaps that have a valid right-hand side (rhs)
-    if keymap.rhs and keymap.lhs then
-      print(keymap.lhs .. " -> " .. keymap.rhs)
-    end
-  end
-end, {})
-
--- quit
 keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
--- nvim.bak/lua/plugins/treesitter.lua
+++ nvim.bak/lua/plugins/treesitter.lua
@@ -1,50 +1,63 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    build = ":TSUpdate",
+    config = function()
+        local treesitter = require("nvim-treesitter.configs")
 
-    local treesitter = require("nvim-treesitter.configs")
+        treesitter.setup({
+            highlight = {
+                enable = true,
+                disable = function(lang, buf)
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
+                additional_vim_regex_highlighting = false,
+            },
 
-    treesitter.setup({
-      highlight = {
-        enable = true,
-      },
+            auto_install = false,
+            sync_install = false,
+            modules = {},
+            ignore_install = {},
 
-      indent = { enable = true },
+            indent = { enable = true },
 
-      autotag = {
-        enable = true,
-      },
+            autotag = {
+                enable = true,
+            },
 
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "java",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "lua",
-        "vim",
-        "gitignore",
-        "vimdoc",
-        "luadoc",
-        "sql",
-      },
+            ensure_installed = {
+                "json",
+                "javascript",
+                "typescript",
+                "tsx",
+                "yaml",
+                "html",
+                "css",
+                "java",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "lua",
+                "vim",
+                "gitignore",
+                "vimdoc",
+                "luadoc",
+                "sql",
+            },
 
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    })
-  end,
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+        })
+    end,
 }
